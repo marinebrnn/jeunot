2d0b609363b774a8cd4276c243685abf0783e175
Florimond Manca <florimond.manca@fairness.coop>
2023-10-05 16:23:12 +0100

    Rework implementation of event card location caption (#21)
    * Rework implementation of event card location caption
    
    * Rework tests

--- tests/Integration/Infrastructure/Controller/Event/ListEventsControllerTest.php
+++ tests/Integration/Infrastructure/Controller/Event/ListEventsControllerTest.php
@@ -12,10 +12,10 @@ final class ListEventsControllerTest extends AbstractWebTestCase
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/events?pageSize=20');
-        $li = $crawler->filter('main ul.j-raw-list > li');
-        $event = $li->filter('a');
-        $attendees = $li->filter('small');
-        $location = $li->filter('figure');
+        $li = $crawler->filter('[data-testid="event-list"] li');
+        $eventTitle = $li->filter('h3');
+        $attendees = $li->filter('[data-testid="event-attendees"]');
+        $location = $li->filter('[data-testid="event-location"]');
 
         $this->assertResponseStatusCodeSame(200);
         $this->assertSecurityHeaders();
@@ -24,18 +24,18 @@ final class ListEventsControllerTest extends AbstractWebTestCase
         $this->assertSame(3, $li->count());
 
         $this->assertSame('1 participant', $attendees->eq(0)->text());
-        $this->assertSame('Dîner au restaurant', $event->eq(0)->text());
-        $this->assertSame('http://localhost/events/a6ed00e5-a566-4115-a547-01378baba9b1', $event->eq(0)->link()->getUri());
-        $this->assertSame('Paris', $location->eq(0)->text());
+        $this->assertSame('Dîner au restaurant', $eventTitle->eq(0)->text());
+        $this->assertSame('http://localhost/events/a6ed00e5-a566-4115-a547-01378baba9b1', $eventTitle->eq(0)->filter('a')->link()->getUri());
+        $this->assertSame('Paris', $li->eq(0)->filter('figcaption')->text());
 
         $this->assertSame('2 participants', $attendees->eq(1)->text());
-        $this->assertSame('http://localhost/events/89f72b23-55e9-4975-b640-da24890095b7', $event->eq(1)->link()->getUri());
-        $this->assertSame('Balade à vélo en tandem', $event->eq(1)->text());
+        $this->assertSame('Balade à vélo en tandem', $eventTitle->eq(1)->text());
+        $this->assertSame('http://localhost/events/89f72b23-55e9-4975-b640-da24890095b7', $eventTitle->eq(1)->filter('a')->link()->getUri());
         $this->assertSame('Paris', $location->eq(1)->text());
 
         $this->assertSame('Aucun participant', $attendees->eq(2)->text());
-        $this->assertSame('http://localhost/events/f1f992d3-3cf5-4eb2-9b83-f112b7234613', $event->eq(2)->link()->getUri());
-        $this->assertSame('Balade et pique-nique en forêt de Chevreuse', $event->eq(2)->text());
+        $this->assertSame('Balade et pique-nique en forêt de Chevreuse', $eventTitle->eq(2)->text());
+        $this->assertSame('http://localhost/events/f1f992d3-3cf5-4eb2-9b83-f112b7234613', $eventTitle->eq(2)->filter('a')->link()->getUri());
         $this->assertSame('Saint Remy les Chevreuses', $location->eq(2)->text());
     }
 
@@ -43,24 +43,24 @@ final class ListEventsControllerTest extends AbstractWebTestCase
     {
         $client = $this->login();
         $crawler = $client->request('GET', '/events?pageSize=20');
-        $li = $crawler->filter('main ul.j-raw-list > li');
-        $event = $li->filter('a');
-        $attendees = $li->filter('small');
+        $li = $crawler->filter('[data-testid="event-list"] li');
+        $eventTitle = $li->filter('h3');
+        $attendees = $li->filter('[data-testid="event-attendees"]');
 
         $this->assertResponseStatusCodeSame(200);
         $this->assertSecurityHeaders();
         $this->assertSame('Événements', $crawler->filter('h1')->text());
         $this->assertMetaTitle('Événements - Jeunot', $crawler);
-        $this->assertSame(3, $crawler->filter('main ul.j-raw-list > li')->count());
+        $this->assertSame(3, $crawler->filter('[data-testid="event-list"] li')->count());
 
         $this->assertSame('1 participant - Inscrit·e !', $attendees->eq(0)->text());
-        $this->assertSame('Dîner au restaurant', $event->eq(0)->text());
+        $this->assertSame('Dîner au restaurant', $eventTitle->eq(0)->text());
 
         $this->assertSame('2 participants - Inscrit·e !', $attendees->eq(1)->text());
-        $this->assertSame('Balade à vélo en tandem', $event->eq(1)->text());
+        $this->assertSame('Balade à vélo en tandem', $eventTitle->eq(1)->text());
 
         $this->assertSame('Aucun participant', $attendees->eq(2)->text());
-        $this->assertSame('Balade et pique-nique en forêt de Chevreuse', $event->eq(2)->text());
+        $this->assertSame('Balade et pique-nique en forêt de Chevreuse', $eventTitle->eq(2)->text());
     }
 
     public function testBadPageParameter(): void
