7c304cc56cec932c1d9f1d02543d88fa5390142e
florimondmanca <florimond.manca@fairness.coop>
2023-10-09 13:56:44 +0200

    Implement mobile menu

--- assets/customElements/index.js
+++ assets/customElements/index.js
@@ -1,3 +1,5 @@
 import cardListItem from "./cardListItem";
+import dialog from "./dialog";
 
 customElements.define("j-card-li", cardListItem, { extends: "li" });
+customElements.define("j-dialog", dialog, { extends: "dialog" });
