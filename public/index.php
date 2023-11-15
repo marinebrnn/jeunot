e6c9c25f28a38b2450b9df2ee5cff2929767a9b3
mmarchois <mathieu.marchois@gmail.com>
2023-11-15 10:58:30 +0100

    [revert][debug scalingo] Try to stop execution

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-die('in');
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
