4f709ed41454422587a74481e288f4236b1b23f9
florimondmanca <florimond.manca@fairness.coop>
2023-12-13 17:44:31 +0100

    Fix home button hover, fix home link hover, add password display button, add required indicator

--- assets/customElements/index.js
+++ assets/customElements/index.js
@@ -1,5 +1,7 @@
 import clickableCard from "./clickableCard";
 import navButton from "./navButton";
+import passwordInput from "./passwordInput";
 
 customElements.define("j-clickable-card", clickableCard);
 customElements.define("j-nav-button", navButton, { extends: "button" });
+customElements.define("j-password-input", passwordInput);
